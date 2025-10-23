ff2dd69baa2391ecd859b5ba0388d9e51f2cd718
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-10-23 19:41:22 +0300

    Community.svelte component

---
+++ client/src/lib/components/communities/Community.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { useCommunityState } from "$lib/states/communityState.svelte.js";
+
+    let communityState = useCommunityState();
+
+    let { communityId } = $props();
+
+    let community = communityState.getOne(communityId);
+</script>
+
+<h1>{community ? community.name : "Loading..."}</h1>
+<p>{community ? community.description : ""}</p>
