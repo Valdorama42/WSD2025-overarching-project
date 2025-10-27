35f77b4e434a403026c42733fdccdc5d0db30f56
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-10-27 14:20:36 +0200

    working version for Posts page

---
+++ client/src/routes/communities/[communityId]/posts/[postId]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Post from "$lib/components/posts/Post.svelte";
+
+    let { data } = $props();
+
+    let communityId = parseInt(data.communityId);
+    let postId = parseInt(data.postId);
+</script>
+
+<Post communityId={communityId} postId={postId} />
