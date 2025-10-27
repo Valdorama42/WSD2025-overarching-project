7d7cbce05b642f1aff388397b4a6b74c924c25cd
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-10-27 14:17:29 +0200

    Post element and routes path

---
+++ client/src/lib/components/posts/Post.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { usePostState } from "$lib/states/postState.svelte.js";
+
+    let postState = usePostState();
+
+    let { communityId, postId } = $props();
+
+    let post = postState.getPost(communityId, postId);
+</script>
+
+<h1>{post ? post.title : "Loading..."}</h1>
+<p>{post ? post.content : ""}</p>
