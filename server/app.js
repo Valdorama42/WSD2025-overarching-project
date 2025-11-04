16e2c203533c2702a10afd0288dbc5e9e67d1937
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-11-04 15:40:34 +0200

    app.js routing

--- server/app.js
+++ server/app.js
@@ -1,26 +1,17 @@
 import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
-// importing database client
-import postgres from "postgres";
+import * as communityController from "./controllers/communityController.js";
 
 const app = new Hono();
-// creating an instance of the database client
-const sql = postgres();
 
 app.use("/*", cors());
 app.use("/*", logger());
 
-let visits = 0;
-app.get("/api/visits", (c) => {
-  visits++;
-  return c.json({ visits });
-});
+app.post("/api/communities", communityController.create);
+app.get("/api/communities", communityController.readAll);
+app.get("/api/communities/:communityId", communityController.readOne);
+app.delete("/api/communities/:communityId", communityController.deleteOne);
 
-// retrieving todos from database on requests to /api/todos
-app.get("/api/todos", async (c) => {
-  const todos = await sql`SELECT * FROM todos`;
-  return c.json(todos);
-});
 
 export default app;
