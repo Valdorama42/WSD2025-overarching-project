2e203aaeb48e2f4cfb296d2a5bac6f8f706a8304
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-11-04 18:27:13 +0200

    app.js routing. Working solution for OAP step 5

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import { Hono } from "@hono/hono";
 import { cors } from "@hono/hono/cors";
 import { logger } from "@hono/hono/logger";
 import * as communityController from "./controllers/communityController.js";
+import * as postController from "./controllers/postController.js";
 
 const app = new Hono();
 
@@ -12,6 +13,11 @@ app.post("/api/communities", communityController.create);
 app.get("/api/communities", communityController.readAll);
 app.get("/api/communities/:communityId", communityController.readOne);
 app.delete("/api/communities/:communityId", communityController.deleteOne);
+
+app.post("/api/communities/:communityId/posts", postController.create);
+app.get("/api/communities/:communityId/posts", postController.readAll);
+app.get("/api/communities/:communityId/posts/:postId", postController.readOne);
+app.delete("/api/communities/:communityId/posts/:postId", postController.deleteOne);
 
 
 export default app;
