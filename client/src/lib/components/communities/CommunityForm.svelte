0526c33bea3e760bec1b0aeadedb5000771b674e
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-11-07 09:35:52 +0200

    Changed the adding functionality to a form that interacts with the server side when submitted

---
+++ client/src/lib/components/communities/CommunityForm.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { useCommunityState } from "$lib/states/communityState.svelte.js";
+
+    let communityState = useCommunityState();
+
+    const addCommunity = (e) => {
+        e.preventDefault();
+
+        const community = Object.fromEntries(new FormData(e.target));
+        communityState.addCommunity(community);
+        e.target.reset();
+    };
+
+</script>
+
+<form onsubmit="{addCommunity}">
+    <div>
+        <label>Community Name:
+            <input 
+                type="text" 
+                id="name" 
+                name="name" 
+                placeholder="Community name" 
+            />
+        </label>
+    </div>
+    <div>
+        <label>Description:
+            <textarea 
+                id="description" 
+                name="description" 
+                placeholder="Community description">
+            </textarea>
+        </label>
+    </div>
+    <button type="submit">Add Community</button>
+</form>
