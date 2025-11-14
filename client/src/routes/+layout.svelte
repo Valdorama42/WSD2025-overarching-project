b8eb0be65d5a047566f4381cfa555e119e131e15
valtteriahonen <valtteri.ahonen@aalto.fi>
2025-11-14 09:15:00 +0200

    updated home page to show content based on authentication

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { useAuthState } from "$lib/states/authState.svelte.js";
+    
+    let { children } = $props();
+
+    const authState = useAuthState();
+</script>
+
+<header>
+    <a href="/">Home</a>
+    
+    {#if authState.user}
+        <div>
+            <span>Hello, {authState.user.email}!</span>
+            <button onclick={() => authState.logout()}>Logout</button>
+        </div>
+    {:else}
+        <ul>
+            <li><a href="/auth/login">Login</a></li>
+            <li><a href="/auth/register">Register</a></li>
+        </ul>
+    {/if}
+</header>
+
+{@render children()}
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <script>
-    
+    import { useAuthState } from "$lib/states/authState.svelte.js";
+    const authState = useAuthState();
 </script>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
   
-<h1>Visit count: </h1>
+<h1>Welcome to the home page!</h1>
+
+{#if authState.user}
+    <a href="/communities">Go to communities</a>
+{:else}
+    <p>Please <a href="/auth/login">login</a> or <a href="/auth/register">register</a> to continue.</p>
+{/if}
